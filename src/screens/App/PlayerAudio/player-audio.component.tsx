b6d5199d59d4bd95c0d66d75e39a03a999355ebf
victor ciechovicz <victor09ciechovicz@outlook.com>
2023-01-17 01:30:28 -0300

    create player of audio and type .png global

---
+++ src/screens/App/PlayerAudio/player-audio.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import * as S from './player-audio.styles';
+import * as T from './player-audio.types';
+
+const PlayerAudio: React.FC<T.PlayerAudioProps> = () => {
+  return <S.Container />;
+};
+
+export default PlayerAudio;
